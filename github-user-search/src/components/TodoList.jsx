616abb9eb8018da21c9db05e7e19ab3d1dd1c60e
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-03 16:59:59 +0300

    Delete github-user-search directory

--- github-user-search/src/components/TodoList.jsx
+++
@@ -1,9 +0,0 @@
-function TodoList({ task }) {
-  return (
-    <div className="TodoList">
-      <h3>{task}</h3>
-      <button>delete</button>
-    </div>
-  );
-}
-export default TodoList;
--- github-user-search/src/components/todo.jsx
+++
@@ -1,8 +0,0 @@
-function TodoTitle() {
-  return (
-    <div className="todoTitle">
-      <h1>MY TODO LIST</h1>
-    </div>
-  );
-}
-export default TodoTitle;
